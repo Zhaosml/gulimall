d092167725b33b497fce5894521df58a34447e56
xiaopang <>
2023-07-14 14:07:52 +0700

    gulimall-product模块-1.1 这里把测试文件，测试了一下mybatis-plus的使用。

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,33 @@
 package com.atguigu.gulimall.product;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class GulimallProductApplicationTests {
+import java.util.List;
 
+@SpringBootTest
+public class GulimallProductApplicationTests {
+	@Autowired
+	BrandService brandService;
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		BrandEntity brandEntity = new BrandEntity();
+		//brandEntity.setName("华为");
+		//brandService.save(brandEntity);
+
+		//brandEntity.setBrandId(1L);
+		//brandEntity.setDescript("华为");
+		//brandService.updateById(brandEntity);
+		//System.out.println("保存成功。。。。");
+
+		List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+		list.forEach((item)->{
+			System.out.println(item);
+		});
 	}
 
 }
