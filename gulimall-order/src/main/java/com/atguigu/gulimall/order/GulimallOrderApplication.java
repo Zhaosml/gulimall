24ae55a30cf9e7c189666ffab432254919ce2ef0
xiaopang <>
2023-07-14 17:00:48 +0700

    全部模块引入 nacos common加入阿里巴巴依赖和nacos依赖

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
@@ -3,12 +3,14 @@ package com.atguigu.gulimall.order;
 import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author 钊思暮想
  * @date 2023/7/14 14:40
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GulimallOrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(GulimallOrderApplication.class);
--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/GulimallWareApplicationTests.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/GulimallWareApplicationTests.java
@@ -2,12 +2,14 @@ package com.atguigu.gulimall.ware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author 钊思暮想
  * @date 2023/7/14 15:13
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GulimallWareApplicationTests {
     public static void main(String[] args) {
         SpringApplication.run(GulimallWareApplicationTests.class);
